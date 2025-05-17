c0f42e251dd8317b0dd88476eec0b31af1bd2fe2
nitinbsw2008 <23f1000506@ds.study.iitm.ac.in>
2025-05-17 16:15:47 +0530

    html setup

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"]) # NOTE: / =  http://192.168.1.42:5000
 def home():
-    return "Hello, World!"
+    return render_template("login.html")
 
 if __name__ == "__main__":
+    app.debug = True
     app.run(host="0.0.0.0")
