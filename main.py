fdebbe396dd854e9b5edd7a24f1770f506a55f5c
nitinbsw2008 <23f1000506@ds.study.iitm.ac.in>
2025-05-17 13:59:49 +0530

    Flask installed

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+import os
+
+app =Flask(_name_)
+
+@app.route("/", methods=["GET"])
+def login():
+    return render_template(("login.html"))
+
+@app.route("/user_signup", methods=["GET"])
+def signup():
+    return render_template("signup.html")
+
+if __name__ == "__main__"
+    app.debug = True
+    app.run(host="0.0.0.0")
+    
